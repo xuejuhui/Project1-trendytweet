c84bbd4849c7e3567e7f41d3778f61c7222fa526
Juhui(Ray) Xue <32085550+xuejuhui@users.noreply.github.com>
2018-01-07 23:44:10 -0800

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  consumer_key: '7NfieIUe9InhsSjWSOnqRRKBo',
-  consumer_secret: 'gBgEoNnV8smLlDXWE2dbv9GrLl9aKBLBgsZHxZ9OI81IqapaFf',
-  access_token: '3068070909-G5BjVgXzjIsJZG8P96NqYAx106JjthMWV9PmJ0w',
-  access_token_secret: 'IEjMg4hA5YDelbBoU5hvmV0dDz7DgyZiJXSXNmzuZEADY'
-}
-module.exports = config
-
